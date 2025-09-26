038e85aa3853f62a798bd9da9c5fcbea6aa07a96
Jana Mohamed <janamohamedd730@gmail.com>
2025-09-26 15:16:22 +0300

    fix 6

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(helmet())
 
 // cors
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
 }));
